59bda167719ef7971259a634df994cac50c1bded
rubens Rodrigues <rubens@schoolguardian.app>
2022-06-17 02:19:14 -0300

    Add worksheet with courses

--- read_excel.py
+++ read_excel.py
@@ -9,6 +9,7 @@
 
 # Imports
 import openpyxl # biblioteca que faz a leitura e gravação em arquivos Excel
+from openpyxl.worksheet.table import Table, TableStyleInfo
 import os       # biblioteca que acessa funções do sistema operacional
 import sys # biblioteca que acessa funções do sistema base, nesse caso nos retorna o sistema operacional
 import shutil # biblioteca para manipular arquivos
@@ -17,39 +18,147 @@ import json
 import requests
 import os
 from dotenv import load_dotenv
+import datetime
 
 
-def createLogFile(log_file_name):
+def createAppFiles(log_file_name, isMac):
     if os.path.exists(log_file_name):
         os.remove(log_file_name)
     else:
         print("The file does not exist")            
     
     load_dotenv()
-    logging.basicConfig(filename=log_file_name,
-                        level=logging.DEBUG)        
+    logging.basicConfig(filename=log_file_name, level=logging.DEBUG)        
 
-def createResult(var_wks, var_loc, var_wkb, isMac):
-    var_wkl = 1 # um contador de linhas para a planilha resultante
+    readFolder = var_loc + '\\lidos\\'
+    resultFolder = var_loc + '\\resultado\\'
+    if(isMac):
+        readFolder = var_loc + '/lidos/'
+        resultFolder = var_loc + '/resultado/'
+
+    if(os.path.isdir(readFolder) == False):
+        os.mkdir(readFolder) # diretorio que recebera os arquivos que foram lidos
+    if(os.path.isdir(resultFolder) == False):  # se não existe o diretório resultado
+        os.mkdir(resultFolder) # cria diretorio que contem o arquivo resultante
+
+    if os.path.exists(getResultFile(isMac)):
+        os.remove(getResultFile(isMac))
+
+def getResultFile(isMac):    
+    if(isMac):
+        return var_loc + '/resultado/' + 'resultado.xlsx'        
+    else:
+        return var_loc + '\\resultado\\' + 'resultado.xlsx'
+
+
+def createResult(var_loc, isMac):
+    print("Criando Restul")
+    wb = openpyxl.Workbook()    
+    mainWks = wb.active
+    
     # Preenchendo o cabeçalho da planilha resultante
-    var_wks.cell(row=var_wkl, column=1).value = 'Nome do Arquivo'
-    var_wks.cell(row=var_wkl, column=2).value = 'Mes'
-    var_wks.cell(row=var_wkl, column=3).value = 'Ano'
-    var_wks.cell(row=var_wkl, column=4).value = 'Nome do Professor'
-    var_wks.cell(row=var_wkl, column=5).value = 'Contrato do Professor'
-    var_wks.cell(row=var_wkl, column=6).value = 'Curso'
-    var_wks.cell(row=var_wkl, column=7).value = 'Turma'
-    var_wks.cell(row=var_wkl, column=8).value = 'Fase'
-    var_wks.cell(row=var_wkl, column=9).value = 'Atividade'
-    var_wks.cell(row=var_wkl, column=10).value = 'Data'
-    var_wks.cell(row=var_wkl, column=11).value = 'Quantidade de Horas'
-    var_wks.cell(row=var_wkl, column=12).value = 'Observacao'
+    mainWks.cell(row=1, column=1).value = 'Nome do Arquivo'
+    mainWks.cell(row=1, column=2).value = 'Mes'
+    mainWks.cell(row=1, column=3).value = 'Ano'
+    mainWks.cell(row=1, column=4).value = 'Nome do Professor'
+    mainWks.cell(row=1, column=5).value = 'Contrato do Professor'
+    mainWks.cell(row=1, column=6).value = 'Curso'
+    mainWks.cell(row=1, column=7).value = 'Turma'
+    mainWks.cell(row=1, column=8).value = 'Fase'
+    mainWks.cell(row=1, column=9).value = 'Atividade'
+    mainWks.cell(row=1, column=10).value = 'Data'
+    mainWks.cell(row=1, column=11).value = 'Quantidade de Horas'
+    mainWks.cell(row=1, column=12).value = 'Observacao'
     logging.debug('BEFORE SAVE ' + var_loc)
 
-    if(isMac):
-        var_wkb.save(var_loc + '/resultado/' + 'resultado.xlsx') # salva o workbook
-    else:
-        var_wkb.save(var_loc + '\\resultado\\' + 'resultado.xlsx') # salva o workbook
+    for courses in array_courses:
+        wks = wb.create_sheet(title=courses)
+        wks.cell(row=1, column=1).value = 'EMPRESA'
+        wks.cell(row=1, column=2).value = 'CAMPUS'
+        wks.cell(row=1, column=3).value = 'TIPO'
+        wks.cell(row=1, column=4).value = 'UNIDADE / DEPARTAMENTO'
+        wks.cell(row=1, column=5).value = 'SEGMENTO'
+        wks.cell(row=1, column=6).value = 'DETALHE (CURSO)'
+        wks.cell(row=1, column=7).value = 'Tipo Contrato'
+        wks.cell(row=1, column=8).value = 'Nº Matrícula (CLT) | Contato (PJ/RPA)'
+        wks.cell(row=1, column=9).value = 'Responsável - Nome Completo'
+        wks.cell(row=1, column=10).value = 'Modalidade'
+        wks.cell(row=1, column=11).value = 'Turma'
+        wks.cell(row=1, column=12).value = 'Disciplina / Fase'
+        wks.cell(row=1, column=13).value = 'Num. Cap.'
+        wks.cell(row=1, column=14).value = 'Capítulo'
+        wks.cell(row=1, column=15).value = 'DATA (Entrega / Correção)'
+        wks.cell(row=1, column=16).value = 'CONTROLE INTERNO - LAUDAS / HORAS (não usar)'
+        wks.cell(row=1, column=17).value = 'Núm. págs / horas'
+        wks.cell(row=1, column=18).value = 'Data de cadastro (interno)'        
+
+    wb.save(getResultFile(isMac))  # salva o workbook
+
+
+def mountInfo(wb, array_send, line_controller, fileName, month, year, prof, contract, course, class_txt, phase, activity, 
+                final_data, final_machine_data, hours, obs):
+    line_send = {}
+    line_send['nome_arquivo'] = str(fileName)
+    line_send['Mes'] =str(month)
+    line_send['Ano'] = str(year)
+    line_send['Professor'] = str(prof)
+    line_send['Contrato'] = str(contract)
+    line_send['Curso'] =str(course)
+    line_send['Turma'] = str(class_txt)
+    line_send['Fase'] = str(phase)
+    line_send['Atividade'] = str(activity)
+    line_send['Data'] = str(final_machine_data)
+    line_send['Quantidade'] = str(hours)
+    line_send['Observacao'] = str(obs)
+
+    array_send.append(line_send)
+
+    wkLine = line_controller['all']
+
+    work_sheet = wb['Sheet']
+
+    work_sheet.cell(row=wkLine, column=1).value = "VSTP"
+    work_sheet.cell(row=wkLine, column=2).value = "FIAP ON"
+    work_sheet.cell(row=wkLine, column=3).value = "EDUCACIONAL / PROFESSORES"
+    work_sheet.cell(row=wkLine, column=4).value = "MBA ON"
+    work_sheet.cell(row=wkLine, column=5).value = "Especialização"
+    work_sheet.cell(row=wkLine, column=6).value = str(course).upper()
+    work_sheet.cell(row=wkLine, column=7).value = "**Copiar"
+    work_sheet.cell(row=wkLine, column=8).value = "**Copiar"
+    work_sheet.cell(row=wkLine, column=9).value = str(prof).upper()
+    work_sheet.cell(row=wkLine, column=10).value = str(activity)
+    work_sheet.cell(row=wkLine, column=11).value = str(class_txt).replace(" ", "")
+    work_sheet.cell(row=wkLine, column=12).value = str(phase)
+    work_sheet.cell(row=wkLine, column=13).value = ""
+    work_sheet.cell(row=wkLine, column=14).value = ""
+    work_sheet.cell(row=wkLine, column=15).value = str(final_data)
+    work_sheet.cell(row=wkLine, column=16).value = "**Copiar"
+    work_sheet.cell(row=wkLine, column=17).value = str(hours).replace(".", ",")
+    work_sheet.cell(row=wkLine, column=18).value = ""
+
+    wkLine = line_controller[course]
+    work_sheet = wb[course]
+    work_sheet.cell(row=wkLine, column=1).value = "VSTP"
+    work_sheet.cell(row=wkLine, column=2).value = "FIAP ON"
+    work_sheet.cell(row=wkLine, column=3).value = "EDUCACIONAL / PROFESSORES"
+    work_sheet.cell(row=wkLine, column=4).value = "MBA"
+    work_sheet.cell(row=wkLine, column=5).value = "Especialização"
+    work_sheet.cell(row=wkLine, column=6).value = str(course).upper()
+    work_sheet.cell(row=wkLine, column=7).value = "**Copiar"
+    work_sheet.cell(row=wkLine, column=8).value = "**Copiar"
+    work_sheet.cell(row=wkLine, column=9).value = str(prof).upper()
+    work_sheet.cell(row=wkLine, column=10).value = str(activity)
+    work_sheet.cell(row=wkLine, column=11).value = str(class_txt).replace(" ", "")
+    work_sheet.cell(row=wkLine, column=12).value = str(phase)
+    work_sheet.cell(row=wkLine, column=13).value = ""
+    work_sheet.cell(row=wkLine, column=14).value = ""
+    work_sheet.cell(row=wkLine, column=15).value = str(final_data)
+    work_sheet.cell(row=wkLine, column=16).value = "**Copiar"
+    work_sheet.cell(row=wkLine, column=17).value = str(hours).replace(".", ",")
+    work_sheet.cell(row=wkLine, column=18).value = ""
+
+    line_controller['all'] += 1
+    line_controller[course] += 1
 
 def sendPowerBI(array_send,wb):
     logging.debug("Array to Send")
@@ -88,42 +197,28 @@ var_naq = '' # variável para armazenar o nome do arquivo atual
 var_col = 0 # um contador genérico para contar as colunas
 var_lin = 0 # um contador genérico para contar as linhas
 var_ctr = 0 # um contador genérico, afinal, todo programa precisa de um
-var_wkb = openpyxl.Workbook()
-var_wks = var_wkb.active
 var_wkl = 0 # um contador de linhas para o Workbook
+line_controller = {}
+array_courses = ['GTIO', 'AOJO', 'ASOO', 'ABDO', 'DTSO', 'BDTO', 'DGO', 'NGO', 'BIO', 'SGO', 'SCJO'] 
 
-createLogFile('reading.log')
+isMac = False
+# verifica se o sistema operacional é Linux ou MacOS
+if(var_os == 'linux' or var_os == 'linux2' or var_os == 'darwin'):
+    print('Is Mac')    
+    isMac = True
 
+#cria os arquivos e pastas utilizadas no app
+createAppFiles('reading.log', isMac)
+logging.debug("Iniciando a Importacao")
 
 # verifica se os diretorios resultantes existem, se não, os cria
-if(var_os == 'linux' or var_os == 'linux2' or var_os == 'darwin'): # verifica se o sistema operacional é Linux ou MacOS
-    logging.debug('IS MAC')
-    if(os.path.isdir(var_loc + '/lidos/') == False):
-        os.mkdir(var_loc + '/lidos/') # diretorio que recebera os arquivos que foram lidos
-    if(os.path.isdir(var_loc + '/resultado/') == False): # se não existe o diretório resultado
-        os.mkdir(var_loc + '/resultado/') # cria diretorio que contem o arquivo resultante
-  
-    if(os.path.isfile(var_loc + '/resultado/' + 'resultado.xlsx') == False):
-        createResult(var_wks, var_loc, var_wkb, True)
-    else:       
-        logging.debug(os.path.isfile(var_loc + '/resultado/' + 'resultado.xlsx'))
-        logging.debug('AQUI ' + var_loc)
-        var_wkb = openpyxl.load_workbook(var_loc + '/resultado/' + 'resultado.xlsx')
-        var_wks = var_wkb.active # criando a sheet que receberá os dados
-        var_wkl = var_wks.max_row
-
-elif var_os == 'win32': # verifica se o sistema operacional é Windows
-    if(os.path.isdir(var_loc + '\\lidos\\') == False):
-        os.mkdir(var_loc + '\\lidos\\') # cria o diretorio que recebera os arquivos que foram lidos
-    if(os.path.isdir(var_loc + '\\resultado\\') == False):
-        os.mkdir(var_loc + '\\resultado\\') # cria o diretorio que contem o arquivo resultante
+if(os.path.isfile(getResultFile(isMac)) == False):
+    #Cria a planilha de resultado    
+    createResult(var_loc, isMac)
     
-    if(os.path.isfile(var_loc + '\\resultado\\' + 'resultado.xlsx') == False):
-        createResult(var_wks, var_loc, var_wkb, False)
-    else:        
-        var_wkb = openpyxl.load_workbook(var_loc + '\\resultado\\' + 'resultado.xlsx')
-        var_wks = var_wkb.active # criando a sheet que receberá os dados
-        var_wkl = var_wks.max_row
+#abre a planilha de resultado
+var_wkb = openpyxl.load_workbook(getResultFile(isMac))
+var_wks = var_wkb.active  # criando a sheet que receberá os dados
 
 
 for files in var_dir: # lendo todos os arquivos que estão armazenados no diretório
@@ -132,6 +227,11 @@ for files in var_dir: # lendo todos os arquivos que estão armazenados no diret�
             var_flt.append(var_loc_read + '/' + files) # grava o nome do arquivo no array de nomes
         elif var_os == 'win32': # verifica se o sistema operacional é Windows
             var_flt.append(var_loc_read + '\\' + files) # grava o nome do arquivo no array de nomes
+
+#mount lines Controller
+line_controller['all'] = 1
+for course in array_courses:
+    line_controller[course] = 2
 
 logging.debug("ARQUIVOS Excel a ler")
 logging.debug(var_flt)
@@ -150,7 +250,6 @@ for wb in var_flt:
 
     var_lin = 8 # contador começa em 8 por ser a primeira linha que contém informações sobre as atividades
     var_col = 1 # contador começa em 1 por ser a primeira coluna que contém informações sobre as atividades
-    ind_array = 0
     while var_lin < 70: # iterando sobre a tabela com os dados das atividades reportadas        
         var_check_end = worksheet.cell(row=var_lin, column=6).value # lendo o curso que o professor indicou        
         if (isinstance(var_check_end, str) and "SUM" in var_check_end): # Checa se o cursor chegou no final da planilha
@@ -167,38 +266,19 @@ for wb in var_flt:
                 var_col = 1 # resetando o contador de colunas
                 var_lin = var_lin + 1 # pulando para a próxima linha
 
-                line_send = {}
-                line_send['nome_arquivo'] = str(var_naq)
-                line_send['Mes'] =str(var_mon)
-                line_send['Ano'] = str(var_yea)
-                line_send['Professor'] = str(var_prf)
-                line_send['Contrato'] = str(var_tct)
-                line_send['Curso'] =str(var_crs)
-                line_send['Turma'] =str(var_trm)
-                line_send['Fase'] = str(var_fas)
-                line_send['Atividade'] = str(var_atv)
-                line_send['Data'] = str(var_dat)
-                line_send['Quantidade'] =str(var_qhr)
-                line_send['Observacao'] = str(var_obs)
+                #Trabalhando com a data 
+                final_var_data = ""
+                final_machine_data = ""
+                if (isinstance(var_dat, datetime.datetime)):
+                    final_var_data = var_dat.strftime("%d/%m/%Y")
+                    final_machine_data = var_dat.strftime("%Y-%m-%d")
+                elif (isinstance(var_dat, str)):
+                    final_var_data = final_machine_data = var_dat[0: 10]
+                else:
+                    final_var_data = final_machine_data = var_dat
 
-                array_send.append(line_send)
-
-                var_wkl = var_wkl + 1
-
-                var_wks.cell(row=var_wkl, column=1).value = str(var_naq)
-                var_wks.cell(row=var_wkl, column=2).value = str(var_mon)
-                var_wks.cell(row=var_wkl, column=3).value = str(var_yea)
-                var_wks.cell(row=var_wkl, column=4).value = str(var_prf)
-                var_wks.cell(row=var_wkl, column=5).value = str(var_tct)
-                var_wks.cell(row=var_wkl, column=6).value = str(var_crs)
-                var_wks.cell(row=var_wkl, column=7).value = str(var_trm)
-                var_wks.cell(row=var_wkl, column=8).value = str(var_fas)
-                var_wks.cell(row=var_wkl, column=9).value = str(var_atv)
-                var_wks.cell(row=var_wkl, column=10).value = str(var_dat)
-                var_wks.cell(row=var_wkl, column=11).value = str(var_qhr)
-                var_wks.cell(row=var_wkl, column=12).value = str(var_obs)
-
-                ind_array += 1
+                mountInfo(var_wkb, array_send, line_controller, var_naq, var_mon, var_yea, var_prf, var_tct, var_crs,
+                          var_trm, var_fas, var_atv, final_var_data, final_machine_data, var_qhr, var_obs)
             else:
                 var_lin = var_lin + 1 # pulando para a próxima linha
     
